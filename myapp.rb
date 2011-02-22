512c9ac4ea171acd55373105c849b30261bddb64
Cong Sun <cong.c.sun@gmail.com>
2011-02-21 21:00:55 -0600

    a whole evening's work & demo... grabbing stuff from twitter rss and displaying them in the app.

--- myapp.rb
+++ myapp.rb
@@ -1,8 +1,28 @@
 require 'sinatra'
+require 'open-uri'
+require 'rss'
 
 get '/hello' do
   @title="My First Sinatra App"
   @greeting="Hello world! The time is now #{Time.now}"
   erb :hello 
+end
+
+
+
+
+get %r{/twitter/([\w]+)} do
+  feedurl="http://search.twitter.com/search.rss?q=#{params[:captures].first}"
+  feedcontent=""
+  open(feedurl) do |s| feedcontent = s.read end
+  rss = RSS::Parser.parse(feedcontent,false)
+
+  @feedtitle=rss.channel.title
+  @feedlink=rss.channel.link
+  @feeditems=rss.channel.items
+  @title="Twitter feed for " + @feedtitle
+
+  erb :twitter
+
 end
 
